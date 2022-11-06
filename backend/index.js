31b5acc9a01a607e7c1456e14fc5e794262cb110
Max Phillips <max@maxtop.local>
2022-11-06 22:39:09 +1100

    setup backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
-import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import router from './routes/routes.js';
-import './db.js';
+import { connect } from './db.js';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('tiny'));
-
-// Import the routes from routes.js
-app.use('/', router);
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+async function run() {
+  // Setup mongodb connection
+  await connect();
+
+  // Import the routes from routes.js
+  app.use('/', router);
+
+  app.listen(PORT);
+}
+
+run().then(() => {
   console.log(`Server is listening on port ${PORT}`);
 });
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,46 @@
 import { Router } from 'express';
+import { client } from '../db.js';
 
 const router = Router();
+const db = client.db('sus');
+const statsCollections = db.collection('stats');
+
+function seedStats() {
+  if (statsCollections.countDocuments() === 0) {
+    statsCollections.insertMany([
+      {
+        crewmate: 'Red',
+      },
+      {
+        crewmate: 'Blue',
+      },
+      {
+        crewmate: 'Green',
+      },
+    ]);
+  }
+}
+
+seedStats();
+
+router.get('/results', async (req, res) => {
+  const stats = await statsCollections.find().toArray();
+  res.json(stats);
+});
+
+router.post('/vote', async (req, res) => {
+  const { crewmate } = req.body;
+  const stats = await statsCollections.findOne({ crewmate });
+
+  if (!stats || !crewmate)
+    return res.status(404).json({ error: 'No crewmate provided.' });
+
+  if (stats) {
+    await statsCollections.updateOne({ crewmate }, { $inc: { votes: 1 } });
+  } else {
+    await statsCollections.insertOne({ crewmate, votes: 1 });
+  }
+  res.json({ message: 'Vote cast' });
+});
 
 export default router;
